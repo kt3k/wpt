e17ff4d215bff782a734ba64ebdd576a48b8379f
Joey Arhar <jarhar@chromium.org>
2021-01-13 17:28:52 -0800

    Update <link> pseudo selector WPTs
    In these spec PRs [1][2], the <link> element will no longer match :link,
    :visited, or :any-link, and it will no longer navigate the page when
    clicked.
    
    [1] https://github.com/whatwg/html/pull/6269
    [2] https://github.com/w3c/csswg-drafts/pull/5839
    
    Bug: 611093
    Change-Id: I7234eb6024450e28c1ca6ec1ede7fdfc8f2ece52

--- html/semantics/selectors/pseudo-classes/utils.js
+++ html/semantics/selectors/pseudo-classes/utils.js
@@ -9,6 +9,17 @@ function getElementsByIds(ids) {
 function testSelectorIdsMatch(selector, ids, testName) {
   test(function(){
     var elements = document.querySelectorAll(selector);
+    //assert_unreached('one: ' + JSON.stringify([...elements], null, 2) + '\n\ntwo: ' + JSON.stringify(getElementsByIds(ids), null, 2));
+    const one = [...elements];
+    const two = getElementsByIds(ids);
+    console.log('one:');
+    for (let i = 0; i < one.length; i++) {
+      console.log('[' + i + '] ' + one[i]);
+    }
+    console.log('two:');
+    for (let i = 0; i < two.length; i++) {
+      console.log('[' + i + '] ' + two[i]);
+    }
     assert_array_equals([...elements], getElementsByIds(ids));
   }, testName);
 }
